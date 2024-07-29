0ccfff6bde654efc910bb3bc14ef6d7458f242fd
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-07-29 17:45:46 -0300

    Mon Jul 29 05:45:46 PM -03 2024

--- src/main/java/br/dev/henriquekh/senacCourse/App.java
+++ src/main/java/br/dev/henriquekh/senacCourse/App.java
@@ -3,46 +3,48 @@ package br.dev.henriquekh.senacCourse;
 import java.time.LocalDate;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
-        Car car = new Car("Toyota", "Corolla", "preta");
-        Swimmer swimmer = new Swimmer("Vitoria", 19);
+        public static void main(String[] args) throws Exception {
+                System.out.println("Hello World!");
+                Car car = new Car("Toyota", "Corolla", "preta");
+                Swimmer swimmer = new Swimmer("Vitoria", 19);
 
-        car.description();
+                car.description();
 
-        System.out.println(String.format("Nadador(a):\n\tNome: %s\n\tIdade: %d", swimmer.name, swimmer.age));
+                System.out.println(String.format("Nadador(a):\n\tNome: %s\n\tIdade: %d", swimmer.name, swimmer.age));
 
-        Product product = new Product("Sonic The Hedgehog 2 - Com uma linha", Float.POSITIVE_INFINITY, 1);
-        System.out.println(String.format("Produto: %s\n\tPreço: %f\n\tQuantidade: %d", product.getName(),
-                product.getPrice(), product.getQuantity()));
+                Product product = new Product("Sonic The Hedgehog 2 - Com uma linha", Float.POSITIVE_INFINITY, 1);
+                System.out.println(String.format("Produto: %s\n\tPreço: %f\n\tQuantidade: %d", product.getName(),
+                                product.getPrice(), product.getQuantity()));
 
-        Book book = new Book();
-        book.author = "George Orwell";
-        book.title = "1984";
+                Book book = new Book("1984", "George Orwell", 328);
+                book.displayInfo();
 
-        Computer computer = new Computer(true, 0x1000);
-        computer.displayStatus();
+                Computer computer = new Computer(true, 0x1000);
+                computer.displayStatus();
 
-        Movie movie = new Movie("Your Name", new String[] { "Ryunosuke Kamiki",
-                "Mone Kamishiraishi" }, "Makoto Shinkai", 2016, "tt12345678");
-        movie.displayInfo();
+                Movie movie = new Movie("Your Name", new String[] { "Ryunosuke Kamiki",
+                                "Mone Kamishiraishi" }, "Makoto Shinkai", 2016, "tt12345678");
+                movie.displayInfo();
 
-        City city = new City("Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch", 3106);
-        city.setPopulation(3107);
-        System.out.printf("%s (%d habitantes)\n", city.getName(), city.getPopulation());
+                City city = new City("Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch", 3106);
+                city.setPopulation(3107);
+                System.out.printf("%s (%d habitantes)\n", city.getName(), city.getPopulation());
 
-        BankAccount bankAccount = new BankAccount(100);
-        bankAccount.setBalance(bankAccount.getBalance() + 25);
-        System.out.printf("Conta: %s\nSaldo: R$%.02f\n", bankAccount.getId().toString(), bankAccount.getBalance());
+                BankAccount bankAccount = new BankAccount(100);
+                bankAccount.setBalance(bankAccount.getBalance() + 25);
+                System.out.printf("Conta: %s\nSaldo: R$%.02f\n", bankAccount.getId().toString(),
+                                bankAccount.getBalance());
 
-        Course course = new Course("Curso de Git", 31.4);
-        System.out.printf("%s (%d horas)\n", course.getName(), ((int) course.getDurationHours()));
+                Course course = new Course("Curso de Git", 31.4);
+                System.out.printf("%s (%d horas)\n", course.getName(), ((int) course.getDurationHours()));
 
-        Worker worker = new Worker("Art McArtist", 0);
-        System.out.printf("%s (R$%.02f)\n", worker.getName(), worker.getSalary());
+                Worker worker = new Worker("Art McArtist", 0);
+                System.out.printf("%s (R$%.02f)\n", worker.getName(), worker.getSalary());
 
-        Person henrique = new Person("Henrique", "123.456.789-10", LocalDate.parse("2007-04-20"), "Desempregado",
-                "Meio do nada", "Azul");
-        henrique.displayInfo();
-    }
+                Person henrique = new Person("Henrique", "123.456.789-10", LocalDate.parse("2007-04-20"),
+                                "Desempregado",
+                                "Meio do nada", "Azul");
+                henrique.displayInfo();
+
+        }
 }
--- src/main/java/br/dev/henriquekh/senacCourse/Book.java
+++ src/main/java/br/dev/henriquekh/senacCourse/Book.java
@@ -1,6 +1,43 @@
 package br.dev.henriquekh.senacCourse;
 
 public class Book {
-    String title;
-    String author;
+    private String title;
+    private String author;
+    private int pages;
+
+    public Book(String title, String author, int pages) {
+        setAuthor(author);
+        setPages(pages);
+        setTitle(title);
+    }
+
+    private void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private void setPages(int pages) {
+        if (pages < 1)
+            throw new IllegalArgumentException("less then 1 page is not allowed");
+        this.pages = pages;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void displayInfo() {
+        System.err.printf("%s - %s (%d paginas)\n", getTitle(), getAuthor(), getPages());
+    }
 }
